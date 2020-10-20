53e431f5cec0c476e57d384e5fdf397c61501e19
SaiDeeksha29 <saideeksha29@gmail.com>
2020-10-20 20:09:18 +0530

    Added:Test case for calculating fare for multiple rides

--- test/java/InvoiceService/InvolveServiceTest.java
+++ test/java/InvoiceService/InvolveServiceTest.java
@@ -12,7 +12,7 @@ public class InvolveServiceTest {
 	public void setUp() throws Exception {
 		invoiceGenerator = new InvoiceGenerator();
 	}
-	
+
 	@Test
 	public void givenDistanceAndTimeShouldReturnTotalFare() {
 		double distance = 2.5;
@@ -21,12 +21,18 @@ public class InvolveServiceTest {
 		Assert.assertEquals(35, fare, 0.0);
 	}
 
-	// uc1
 	@Test
 	public void givenLessDistanceAndTimeShouldReturnMinimumFare() {
 		double distance = 0.1;
 		int time = 1;
 		double fare = invoiceGenerator.calculateFare(distance, time);
 		Assert.assertEquals(5, fare, 0.0);
+	}
+
+	@Test
+	public void givenMultipleRidesReturnAggregateTotalFare() {
+		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+		double totalFare = invoiceGenerator.calculateFare(rides);
+		Assert.assertEquals(30, totalFare, 0.0);
 	}
 }
